b73d99c792598e2dd7c7b312f167c6905206d29c
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-03-16 17:20:10 -0500

    Create AreaOfPentagon.java

---
+++ AreaOfPentagon.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class AreaOfPentagon {
+  
+	public static void main(String[] args) {
+		
+		// s is a length of the side 
+		// r is radius
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value for radius: ");
+		
+		//establish vars and formulas 
+		double r = input.nextDouble();
+		double s = (2* r * Math.sin(Math.PI/5));
+		double area = ((5 * Math.pow(s, 2)) / (4 * Math.tan(Math.PI/5))); 
+		
+		System.out.printf("The area is %4.2f", area);
+		
+	}
+
+}
