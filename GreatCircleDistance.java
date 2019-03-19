4b774d3aa90bfe962f29c1893b8508d45c169263
SkyWalker9955 <47829986+SkyWalker9955@users.noreply.github.com>
2019-03-19 10:38:01 -0500

    Create GreatCircleDistance.java

---
+++ GreatCircleDistance.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class GreatCircleDistance {
+  
+	public static void main(String[] args) {
+	
+	//latitude (x) and longitude (y) of two points
+	double x1, y1, x2, y2;
+	
+	//Earth radius in km 
+	double r = 6371.01;
+	
+	//Input
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter the latitude of the first point");
+	x1 = input.nextDouble();
+	System.out.println("Enter the longitude of the first point");
+	y1 = input.nextDouble();
+		
+	System.out.println("Enter the latitude of the second point");
+	x2 = input.nextDouble();
+	System.out.println("Enter the longitude of the second point");
+	y2 = input.nextDouble();
+	
+	// d is great circle distance 
+	double d;
+	d = r * Math.acos(Math.sin(Math.toRadians(x1)) * Math.sin(Math.toRadians(x2)) 
+		+ Math.cos(Math.toRadians(x1)) * Math.cos(Math.toRadians(x2)) * 
+		Math.cos(Math.toRadians(y1) - Math.toRadians(y2)));
+	
+	//Output 
+	System.out.printf("The distance between two points is: %4.4f", d);
+	
+	input.close();
+	
+	}
+
+}
